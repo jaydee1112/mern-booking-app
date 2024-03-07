7415f5c8033d2ccf8ebdf825314e837f25e637bc
Junaid Rehman <junaid.rehman@selectquote.com>
2024-03-08 00:50:23 +0500

    new changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-import "dotenv/config";
-import mongoose from "mongoose";
-import userRoutes from "./routes/users";
-import authRoutes from "./routes/auth";
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -12,9 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(7000, () => {
-  console.log("Server Running on 7000");
+  console.log('Server Running on 7000');
 });
