ac3eb4d0fa8270a23100d97919e96d47d3f96224
Junaid Rehman <junaid.rehman@selectquote.com>
2024-03-24 16:17:04 +0500

    allowed cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({ origin: '*' }));
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.use('/api/users', userRoutes);
